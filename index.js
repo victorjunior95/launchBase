994323943997b7128dab4350ea63175ae2ffc7b4
Victor Junior <victorjr.muchacho@gmail.com>
2020-06-16 07:39:24 -0400

    added desafio 01-2

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 // Criar um programa que calcula a média
-// das notas entre os alunos e envia
+// das turmas de alunos e envia
 // mensagem do cálculo da média.
 
 
 // const nome3 = `Diego e ${nome}` // permite a adição de variáveis dentro da string (${})
 
-const aluno01 = 'Junior'
-const notaAluno01 = 1.8
+// ARRAY - Conjunto de Objetos|variáveis
+const alunosDaTurmaA = [
+    {
+        nome: "Junior",
+        nota: 1.8
+    },
+    {
+        nome: "Mayk",
+        nota: 10
+    },
+    {
+        nome: "Diego",
+        nota: 2
+    }
+]
 
-const aluno02 = 'Mayk'
-const notaAluno02 = 10
+const alunosDaTurmaB = [
+    {
+        nome: "Cleiton",
+        nota: 10
+    },
+    {
+        nome: "Robson",
+        nota: 10
+    },
+    {
+        nome: "Siclano",
+        nota: 0
+    }
+]
 
-const aluno03 = 'Diego'
-const notaAluno03 = 2
-
-
-
-
-const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
-
-// Se a média for maior que 5, parabenizar a turma
-if (media > 5) {
-    console.log(`A média foi de ${media}, parabéns!`)
-} else {
-    console.log('A média é menor que 5.')
+function calculaMedia(alunos) {
+    return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 }
 
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+
+function enviaMensagem(media, turma) {
+    // Se a média for maior que 5, parabenizar a turma
+    if (media > 5) {
+            console.log(`A média da turma ${turma} foi de ${media}. Parabéns!`)
+    } else {
+        console.log(`A média da turma ${turma} é menor que 5.`)
+    }
+}
+
+enviaMensagem(media1, 'turmaA')
+enviaMensagem(media2, 'turmaB')
 
